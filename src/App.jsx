4c630e1cd9cbaceaceb5e414de3b27d9f7c35e55
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-21 20:13:30 +0530

    sidebar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import './App.css';
 function App() {
+
+  const [activeForm, setAcitveForm] = useState(false);
+
+  function handleForm(){
+      setAcitveForm(true);
+  }
   
   return (
     <div className="container">
-    sidebar
-    project display area
+    <div id="sidebar">
+      <h2>Your Projects</h2>
+      <button id='addBtn' onClick={handleForm}>+ Add Project</button>
+    </div>
+    <div id="display">
+
+      {activeForm ? <div><h2>Form is here</h2></div> : <div><h2>No Project to show</h2></div> }
+    </div>
     </div>
   )
 }
